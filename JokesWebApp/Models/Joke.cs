032fca5123b1aee82080a1ece53b53967fdf8004
Gazi Akviran <gaziakvrn@gmail.com>
2024-06-01 16:27:39 +0300

    Initial Commit
    Initial Commit

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace JokesWebApp.Models
+{
+	public class Joke
+	{
+		public int Id { get; set; }
+		public string JokeQuestions { get; set; }
+		public string JokeAnswers { get; set; }
+
+		public Joke()
+		{
+		}
+	}
+}
+
